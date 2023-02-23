2f257cc7929e4d966750803c67ccef6762b64311
Wando Borges <wanbrwan@gmail.com>
2023-02-23 13:20:15 -0300

    Projeto, notificação

---
+++ backend/src/test/java/com/wtiinfo/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtiinfo.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
